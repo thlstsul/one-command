6a997457e5bdcc61343f254dc7f2ed3ee439a1fc
THELOSTSOUL <1095533751@qq.com>
2024-11-19 19:00:32 +0800

    Add async feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
-use std::ffi::OsStr;
+#[cfg(not(feature = "async"))]
+include!("blocking.rs");
 
+#[cfg(feature = "async")]
 pub mod blocking;
+
+#[cfg(feature = "async")]
+use std::ffi::OsStr;
 
 /// Execute commands on the Windows platform,
 /// without opening a window to maintain consistency with other system behaviors.
+#[cfg(feature = "async")]
 pub struct Command;
 
+#[cfg(feature = "async")]
 impl Command {
     #[allow(clippy::new_ret_no_self)]
     pub fn new<S: AsRef<OsStr>>(program: S) -> async_process::Command {
